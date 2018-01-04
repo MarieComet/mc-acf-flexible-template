1bd07d767c8915b86de61e0da0e8b86b7c5cbf6f
Marie Comet <hello@mariecomet.fr>
2018-01-04 16:06:11 +0100

    end slashs and space in JS

--- assets/js/mc-acf-ft-template.js
+++ assets/js/mc-acf-ft-template.js
@@ -8,67 +8,67 @@ jQuery(document).ready(function($){
     var MC_ACF_Flexible_Template = acf.ajax.extend({
 
         events: {
-            'click button.mc-open' : '_open_popup',
-            'click button.acf-mc-ft-close' : '_close_popup',
-            'click .acf-mc-ft-import' : '_import_template',
-            'click .acf-mc-ft-save' : '_save_template',
+            'click button.mc-open': '_open_popup',
+            'click button.acf-mc-ft-close': '_close_popup',
+            'click .acf-mc-ft-import': '_import_template',
+            'click .acf-mc-ft-save': '_save_template',
         },
 
-        _open_popup : function(e) {
+        _open_popup: function( e ) {
 
             e.preventDefault();
             e.stopImmediatePropagation();
             // close other popups
-            $('.popup').hide();
+            $( '.popup' ).hide();
             // vars
-            var popup = $( e.$el.next('.popup') );
+            var popup = $( e.$el.next( '.popup' ) );
 
-            if(popup.length) {
-                $(popup).show();
-                $(popup).removeClass('-close');
-                $(popup).addClass('-open');
+            if ( popup.length ) {
+                $( popup ).show();
+                $( popup ).removeClass( '-close' );
+                $( popup ).addClass( '-open' );
             }
         },
 
-        _close_popup : function(e) {
+        _close_popup: function( e ) {
 
             e.preventDefault();
             e.stopImmediatePropagation();
             // vars
 
-            var popup = $( e.$el.parent('.popup') );
+            var popup = $( e.$el.parent( '.popup' ) );
 
-            if(popup.length) {
-                $(popup).hide();
-                $(popup).removeClass('-open');
-                $(popup).addClass('-close');
+            if ( popup.length ) {
+                $( popup ).hide();
+                $( popup ).removeClass( '-open' );
+                $( popup ).addClass( '-close' );
             }
         },
         // Import template
-        _import_template : function(e) {
+        _import_template: function( e ) {
             e.preventDefault();
 
-            var parentFlex = e.$el.closest('.acf-field-flexible-content');
+            var parentFlex = e.$el.closest( '.acf-field-flexible-content' );
 
-            var parentValues = parentFlex.find('.values');
+            var parentValues = parentFlex.find( '.values' );
 
-            var numberLayouts = parentValues.find('.layout').length;
+            var numberLayouts = parentValues.find( '.layout' ).length;
 
-            var error_div = parentFlex.find('.acf-mc-ft-import-error');
-            var succes_div = parentFlex.find('.acf-mc-ft-import-success');
+            var error_div = parentFlex.find( '.acf-mc-ft-import-error' );
+            var succes_div = parentFlex.find( '.acf-mc-ft-import-success' );
 
-            $form = $('form#post');
+            $form = $( 'form#post' );
 
             var template_select = parentFlex.find('.acf-templates-select');
             var selectedTemplate = $(template_select).val();
 
             var data = {
-                action      : 'mc_acf_import_template',
+                action          : 'mc_acf_import_template',
                 acf_templates   : selectedTemplate,
-                number_layout : numberLayouts
+                number_layout   : numberLayouts
             };
 
-            data = acf.prepare_for_ajax(data);
+            data = acf.prepare_for_ajax( data );
 
             // set busy
             acf.validation.busy = 1;
@@ -77,44 +77,44 @@ jQuery(document).ready(function($){
             acf.validation.toggle( $form, 'lock' );
 
             $.post({
-                url: acf.get('ajaxurl'),
+                url: acf.get( 'ajaxurl' ),
                 type: 'post',
                 data: data,
                 dataType: 'json',
                 action: 'mc_acf_import_template',
                 success: function( json ) {
 
-                    if(true === json.success) {
+                    if(  true === json.success ) {
                         
-                        $(error_div).hide();
-                        $(succes_div).text( json.data.message ).show();
-                        var layoutsHtml =  $(json.data.layouts);
+                        $( error_div ).hide();
+                        $( succes_div ).text( json.data.message ).show();
+                        var layoutsHtml =  $( json.data.layouts );
                         // loop on layouts
-                        $.each(layoutsHtml, function(key, value) {
+                        $.each( layoutsHtml, function( key, value ) {
                             // create object for use it later
-                            var newItem = $(value);
+                            var newItem = $( value );
                             // append to parent
                             $( parentValues ).append( newItem );
                             // this action set the field and render correctly tabs, etc.
-                            acf.do_action('append', newItem);
+                            acf.do_action( 'append', newItem );
                             // add -collapsed class, if not all new layouts will be opened
-                            $(newItem).addClass('-collapsed just-added bg-green');
+                            $( newItem ).addClass( '-collapsed just-added bg-green' );
                             // remove the empty div
-                            $(parentFlex).find('.no-value-message').hide();
+                            $( parentFlex ).find( '.no-value-message' ).hide();
                         });
 
                         setTimeout(function(){
-                            $(succes_div).text( '' ).hide();
+                            $( succes_div ).text( '' ).hide();
                             // reset select
-                            $(template_select).val(null).trigger('change');
+                            $( template_select ).val(null).trigger( 'change' );
                             // close other popups
-                            $('.popup').hide();
+                            $( '.popup' ).hide();
                         }, 3000);
 
                     } else {
                         //console.log(json.data.message);
-                        $(succes_div).hide();
-                        $(error_div).text( json.data.message ).show();
+                        $( succes_div ).hide();
+                        $( error_div ).text( json.data.message ).show();
                     }
 
                     // unlock so WP can publish form
@@ -122,49 +122,49 @@ jQuery(document).ready(function($){
                     acf.validation.toggle( $form, 'unlock' );
                 },
                 error: function( json ) {
-                    console.log(json);
+                    console.log( json );
                 }
             });
         },
 
         // Save template
-        _save_template : function(e) {
+        _save_template: function( e ) {
             e.preventDefault();
 
-            var parentFlex = e.$el.closest('.acf-field-flexible-content');
+            var parentFlex = e.$el.closest( '.acf-field-flexible-content' );
 
             var parentGroupKey = parentFlex.attr( 'data-key' );
 
-            var parentValues = parentFlex.find('.values');
+            var parentValues = parentFlex.find( '.values' );
 
-            var template_name_input = parentFlex.find('.acf-mc-ft-template-name');
-            var template_name = $(template_name_input).val();
+            var template_name_input = parentFlex.find( '.acf-mc-ft-template-name' );
+            var template_name = $( template_name_input ).val();
 
-            var template_terms_select = parentFlex.find('.acf-templates-terms-select');
-            var template_terms = $(template_terms_select).val();
+            var template_terms_select = parentFlex.find( '.acf-templates-terms-select' );
+            var template_terms = $( template_terms_select ).val();
 
-            var error_div = parentFlex.find('.acf-mc-ft-save-error');
-            var succes_div = parentFlex.find('.acf-mc-ft-save-success');
+            var error_div = parentFlex.find( '.acf-mc-ft-save-error' );
+            var succes_div = parentFlex.find( '.acf-mc-ft-save-success' );
             
-            $form = $('form#post');
+            $form = $( 'form#post' );
 
-            acf.do_action('validation_begin');
+            acf.do_action( 'validation_begin' );
 
-            var data = acf.serialize(parentValues);
+            var data = acf.serialize( parentValues );
             
             // append AJAX action
             data.action = 'mc_acf_ft_save_template';
             data.mc_acf_template_name = template_name;
 
-            if(parentGroupKey.length) {
+            if ( parentGroupKey.length ) {
                 data.mc_acf_parent_key = parentGroupKey;
             }
 
-            if(template_terms) {
+            if ( template_terms ) {
                 data.mc_acf_template_terms = template_terms;
             }
 
-            data = acf.prepare_for_ajax(data);
+            data = acf.prepare_for_ajax( data );
 
             // set busy
             acf.validation.busy = 1;
@@ -173,27 +173,27 @@ jQuery(document).ready(function($){
             acf.validation.toggle( $form, 'lock' );
 
             $.post({
-                url: acf.get('ajaxurl'),
+                url: acf.get( 'ajaxurl' ),
                 type: 'post',
                 data: data,
                 dataType: 'json',
                 action: 'mc_acf_ft_save_template',
                 success: function( json ) {
-                    if(true === json.success) {
-                        $(error_div).hide();
-                        $(succes_div).text( json.data.message ).show();
+                    if ( true === json.success ) {
+                        $( error_div ).hide();
+                        $( succes_div ).text( json.data.message ).show();
                         setTimeout(function(){
-                            $(succes_div).text( '' ).hide();
+                            $( succes_div ).text( '' ).hide();
                             // reset inputs
-                            $(template_name_input).val('');
-                            $(template_terms_select).val(null).trigger('change');
+                            $( template_name_input ).val( '' );
+                            $( template_terms_select ).val(null).trigger( 'change' );
                             // close other popups
-                            $('.popup').hide();
+                            $( '.popup' ).hide();
                         }, 3000);
                     } else {
                         //console.log(json.data.message);
-                        $(succes_div).hide();
-                        $(error_div).text( json.data.message ).show();
+                        $( succes_div ).hide();
+                        $( error_div ).text( json.data.message ).show();
                     }
                     // unlock so WP can publish form
                     acf.validation.busy = 0;
