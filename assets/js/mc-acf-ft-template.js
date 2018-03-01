c60a3af403640fade7bce6879fea8443968ab788
Marie Comet <hello@mariecomet.fr>
2018-03-01 10:22:13 +0100

    fix select2 not initialized when flexible field not displayed at page load

--- assets/js/mc-acf-ft-template.js
+++ assets/js/mc-acf-ft-template.js
@@ -15,9 +15,18 @@ jQuery(document).ready(function($){
         },
 
         _open_popup: function( e ) {
-
+            
             e.preventDefault();
             e.stopImmediatePropagation();
+
+            // little fix sometimes select are not initialized
+            $( '.mc-acf-ft-select2' ).each( function ( i, obj ) {
+                if ( !$( obj ).hasClass( 'select2-hidden-accessible' ) ) {
+                    console.log('select');
+                    $( obj ).select2();
+                }
+            });
+
             // close other popups
             $( '.popup' ).hide();
             // vars
